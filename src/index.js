11c42c8f8a4ffb97b30447a80a19302f7b5b6945
Islam <islamibakaev@gmail.com>
2021-08-05 00:31:05 +0300

    resource creation and reading

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 require('./db/mongoose')
 const User = require('./models/user')
-// const Task = require('./models/task')
+const Task = require('./models/task')
 
 const app = express()
 
@@ -14,11 +14,38 @@ app.get('/', (_, res) => {
   res.send('hello')
 })
 
+app.get('/users', (req, res) => {
+  return User
+    .find({})
+    .then((users) => res.send(users))
+    .catch((err) => res.status(500).send())
+})
+app.get('/users/:id', (req, res) => {
+return User
+  .findById(req.params.id)
+  .then(user => {
+    if (!user) {
+      return res.status(404).send()
+    }
+    res.send(user)
+  })
+  .catch(err => res.status(500).send())
+})
 app.post('/users', (req, res) => {
   const user = new User(req.body)
   user
     .save()
-    .then(() => res.send(user))
+    .then(() => res.status(201).send(user))
+    .catch(err => {
+      res.status(400).send(err)
+    })
+})
+
+app.post('/tasks', (req, res) => {
+  const task = new Task(req.body)
+  task
+    .save()
+    .then(() => res.status(201).send(task))
     .catch(err => {
       res.status(400).send(err)
     })
