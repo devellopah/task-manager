17697a9cd8bb3f427b2d4cbfbd84ddc90936feaa
Islam <islamibakaev@gmail.com>
2021-08-22 02:45:01 +0300

    securely storing passwords

--- src/models/user.js
+++ src/models/user.js
@@ -1,43 +1,54 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 const validator = require('validator')
 
-const User = mongoose.model('User', {
-  name: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  email: {
-    type: String,
-    trim: true,
-    lowercase: true,
-    required: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error('Email is invalid!')
+const schema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      required: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error('Email is invalid!')
+        }
       }
-    }
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-    minLength: 6,
-    validate(value) {
-      if (value.includes('password')) {
-        throw new Error('Password cannot contain "password"!')
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minLength: 6,
+      validate(value) {
+        if (value.includes('password')) {
+          throw new Error('Password cannot contain "password"!')
+        }
       }
-    }
-  },
-  age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-      if (value < 0) {
-        throw new Error('Age must be a positive number!')
+    },
+    age: {
+      type: Number,
+      default: 0,
+      validate(value) {
+        if (value < 0) {
+          throw new Error('Age must be a positive number!')
+        }
       }
     }
   }
+)
+schema.pre('save', async function(next) {
+  const user = this
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8)
+  }
+  next()
 })
+const User = mongoose.model('User', schema)
 
 module.exports = User
