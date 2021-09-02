5998a8f60eecadbbf1fe234cf3e5ef61d8802887
Islam <islamibakaev@gmail.com>
2021-09-03 00:55:26 +0300

    authenticated task endpoints

--- src/routes/task.js
+++ src/routes/task.js
@@ -3,6 +3,31 @@ const Task = require('../models/task')
 const auth = require('../middleware/auth')
 const router = new express.Router
 
+router.get('/tasks', auth, async (req, res) => {
+  try {
+    await req.user.populate('tasks').execPopulate()
+    if (req.user.tasks.length) {
+      res.send(req.user.tasks)
+    } else {
+      res.status(404).send()
+    }
+  } catch (e) {
+    res.status(500).send()
+  }
+})
+
+router.get('/tasks/:id', auth, async (req, res) => {
+  try {
+    const task = await Task.findOne({ _id: req.params.id, owner: req.user._id })
+    if (!task) {
+      return res.status(404).send()
+    }
+    res.send(task)
+  } catch (e) {
+    res.status(500).send()
+  }
+})
+
 router.post('/tasks', auth, async (req, res) => {
   const task = new Task({
     ...req.body,
@@ -13,6 +38,39 @@ router.post('/tasks', auth, async (req, res) => {
     res.status(201).send(task)
   } catch (e) {
     res.status(400).send(e)
+  }
+})
+
+router.patch('/tasks/:id', auth, async (req, res) => {
+  const updates = Object.keys(req.body)
+  const allowedForUpdate = ['description', 'completed']
+  const isValidOperation = updates.every(item => allowedForUpdate.includes(item))
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid updates!' })
+  }
+  try {
+    const task = await Task.findOne({ _id: req.params.id, owner: req.user._id })
+    if (!task) {
+      return res.status(404).send()
+    }
+    updates.forEach(prop => task[prop] = req.body[prop])
+    await task.save()
+    res.send(task)
+  } catch (e) {
+    res.status(500).send(e)
+  }
+})
+
+router.delete('/tasks/:id', auth, async (req, res) => {
+  try {
+    const task = await Task.findOne({ _id: req.params.id, owner: req.user._id })
+    if (!task) {
+      return res.status(404).send()
+    }
+    await task.remove()
+    res.send(task)
+  } catch (e) {
+    res.status(500).send(e)
   }
 })
 
