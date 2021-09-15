cb6f29e4e4f55e60143a4d69b1eeecf2fce22d0b
Islam <islamibakaev@gmail.com>
2021-09-15 22:31:15 +0300

    add data sorting

--- src/routes/task.js
+++ src/routes/task.js
@@ -5,8 +5,13 @@ const router = new express.Router
 
 router.get('/tasks', auth, async (req, res) => {
   const match = {}
+  const sort = {}
   if (req.query.completed) {
     match.completed = req.query.completed === 'true' ? true : false
+  }
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(':')
+    sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
   }
   try {
     await req.user.populate({
@@ -14,7 +19,8 @@ router.get('/tasks', auth, async (req, res) => {
       match,
       options: {
         limit: parseInt(req.query.limit),
-        skip: parseInt(req.query.skip)
+        skip: parseInt(req.query.skip),
+        sort,
       }
     }).execPopulate()
     if (req.user.tasks.length) {
