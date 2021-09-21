0e6a4dea6881fddea12ca6fe6f92caa9f68a1b83
Islam <islamibakaev@gmail.com>
2021-09-21 20:44:40 +0300

    handle upload errors

--- src/routes/user.js
+++ src/routes/user.js
@@ -112,6 +112,8 @@ router.delete('/users/me', auth, async (req, res) => {
 
 router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
   res.send()
+}, (err, req, res, next) => {
+  res.status(400).send(err.message)
 })
 
 module.exports = router
