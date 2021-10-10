9277c26bb3a3d73464943455a9fe1bf4da4e0efc
Islam <islamibakaev@gmail.com>
2021-10-11 00:48:37 +0300

    added config env files

--- src/models/user.js
+++ src/models/user.js
@@ -76,7 +76,7 @@ userSchema.statics.findByCredentials = async function (email, password) {
 }
 userSchema.methods.generateAuthToken = async function() {
   const user = this
-  const token = jwt.sign({ _id: user._id.toString() }, 'thisismysecret')
+  const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET)
   user.tokens = user.tokens.concat({ token })
   await user.save()
   return token
---
+++ tests/math.test.js
@@ -0,0 +1,17 @@
+const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit } = require('../src/math')
+
+test('should calculate total with tip', () => {
+  const total = calculateTip(10, .3)
+
+  expect(total).toBe(13)
+})
+
+test('Should convert 32 F to 0 C', () => {
+  const result = fahrenheitToCelsius(32)
+  expect(result).toBe(0)
+})
+
+test('Should convert 0 C to 32 F', () => {
+  const result = celsiusToFahrenheit(0)
+  expect(result).toBe(32)
+})
